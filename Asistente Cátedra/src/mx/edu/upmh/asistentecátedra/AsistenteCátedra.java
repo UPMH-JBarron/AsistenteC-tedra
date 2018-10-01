cebeb0e4fcd7a2439a1386063db518643e9afd51
jbarron <jbarron@am8>
2018-10-01 13:57:47 -0500

    Primera clase del proyecto

---
+++ Asistente Cátedra/src/mx/edu/upmh/asistentecátedra/AsistenteCátedra.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package mx.edu.upmh.asistentecátedra;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author jbarron
+ *
+ */
+public class AsistenteCátedra extends Application {
+
+	/* (non-Javadoc)
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
